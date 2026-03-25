09b87b9f6c299b2eda3350aa5687c5ccb5dbf45a
Chris Fenton <fentonc2013@gmail.com>
2026-03-24 22:59:27 -0400

    Re-add config for deployment

---
+++ js/config.js
@@ -0,0 +1,16 @@
+// CONFIG FILE FOR APP
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDLTu2uYUJV8L_mBvROKnj5PLbs4Tpvtfg",
+  authDomain: "bnv-trivia.firebaseapp.com",
+  databaseURL: "https://bnv-trivia-default-rtdb.firebaseio.com",
+  projectId: "bnv-trivia",
+  storageBucket: "bnv-trivia.firebasestorage.app",
+  messagingSenderId: "184749112637",
+  appId: "1:184749112637:web:11585d633860b90ff31954"
+};
+
+const ADMIN_PASSWORD  = "Tr1viaT1me";
+const PLAYER_PASSWORD = "bnv2026";
+const GAME_NAME             = "BNV Music Trivia 2026";
+const QUESTION_TIME_SECONDS = 30;
